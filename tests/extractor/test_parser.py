193774c5492468d8047b9876e2bc1531ef940b54
Narongdej <narongdej@sarnsuwan.com>
2020-11-09 21:14:11 +0700

    Remove unused lines

--- tests/extractor/test_parser.py
+++ tests/extractor/test_parser.py
@@ -1,13 +1,14 @@
 from unittest.mock import patch
 
 import pytest
-from openpyxl import Workbook
 
 from exco.exception import ParsingFailException
+from exco.extractor import Parser
 from exco.extractor.parser.built_in.float_parser import FloatParser
 from exco.extractor.parser.built_in.int_parser import IntParser
 from exco.extractor.parser.built_in.string_parser import StringParser
 from exco.extractor.parser.built_in.value_parser import ValueParser
+from exco.extractor.parser.parsing_result import ParsingResult
 
 
 def test_parser_parse_float():
@@ -33,3 +34,15 @@ def test_value_parser_abstract():
     with pytest.raises(NotImplementedError):
         vp = ValueParser()
         vp.parse_value("a")
+
+
+@patch.multiple(Parser, __abstractmethods__=set())
+def test_parser_abstract():
+    with pytest.raises(NotImplementedError):
+        p = Parser()
+        p.parse(cfp=None)
+
+
+def test_parsing_result_default():
+    pr = ParsingResult(value=1.0, is_ok=False, msg="default test")
+    assert pr.get_value("test") == "test"
