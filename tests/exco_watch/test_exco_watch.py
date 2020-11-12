c13ad871b00ae2df247fdbaad664ee73b84b171e
Piti Ongmongkolkul <piti118@gmail.com>
2020-11-12 12:16:02 +0700

    fix exco watch on osx (#37)
    * fix exco watch on osx

--- tests/exco_watch/test_exco_watch.py
+++ tests/exco_watch/test_exco_watch.py
@@ -5,6 +5,9 @@ from unittest.mock import patch
 from os.path import join, dirname
 import sys
 
+from openpyxl import Workbook
+from openpyxl.comments import Comment
+
 fname = join(dirname(__file__), '../../sample/test/everything/everything_template.xlsx')
 
 
@@ -17,8 +20,7 @@ def test_exo_watch(mocked_sleep, capfd):
 
 def test_handler(capfd):
     handler = ExcoWatchHandler(path=fname)
-    time.sleep(1.1)
-    handler.on_modified(None)
+    handler.run_exco()
     out, err = capfd.readouterr()
     assert 'Latest' in out
 
@@ -31,11 +33,35 @@ def test_handler_double_fire(capfd):
     assert 'Latest' not in out
 
 
-def test_handler_bad_input(capfd):
-    fname = join(dirname(__file__), '../../sample/test/bad_template.xlsx')
+def test_handler_file_never_change(tmpdir, capfd):
     handler = ExcoWatchHandler(path=fname)
     time.sleep(1.1)
     handler.on_modified(None)
+    out, err = capfd.readouterr()
+    assert 'Latest' not in out
+
+
+def simple_workbook():
+    wb = Workbook()
+    wb.active.cell(1, 1).comment = Comment('Hello', 'author')
+    return wb
+
+
+def test_handler_file_changed(tmpdir, capfd):
+    fname = join(tmpdir, 'hello.xlsx')
+    simple_workbook().save(fname)
+    handler = ExcoWatchHandler(path=fname)
+    time.sleep(1.1)
+    simple_workbook().save(fname)
+    handler.on_modified(None)
+    out, err = capfd.readouterr()
+    assert 'Latest' in out
+
+
+def test_handler_bad_input(capfd):
+    fname = join(dirname(__file__), '../../sample/test/bad_template.xlsx')
+    handler = ExcoWatchHandler(path=fname)
+    handler.run_exco()
     out, err = capfd.readouterr()
     assert 'B10' in err
 
