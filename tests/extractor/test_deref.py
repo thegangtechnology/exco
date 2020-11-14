9bdaa8a1e8fe031c876dbe61cfdadfeb3940d28d
Narongdej <narongdej@sarnsuwan.com>
2020-11-15 00:31:51 +0700

    Fix sonarqube duplicate in test_deref.py

--- tests/extractor/test_deref.py
+++ tests/extractor/test_deref.py
@@ -1,41 +1,39 @@
 from os.path import join, dirname
 
 import openpyxl
+import pytest
 
 from exco import ExcelProcessorFactory
 from exco.deref import DerefCell
 from exco.extractor.parser.built_in.int_parser import IntParser
 
 
-def test_deref_text():
+@pytest.fixture
+def dc_1() -> DerefCell:
     sheet_name = "TestSheet"
     workbook = openpyxl.load_workbook(join(dirname(__file__), '../../sample/test/deref/deref_template.xlsx'))
-    text = "<<A2>> and <<B2>>"
-
     dc = DerefCell(sheet_name=sheet_name,
                    workbook=workbook)
+    return dc
 
-    result = dc.deref_text(text)
+
+def test_deref_text(dc_1: DerefCell):
+    text = "<<A2>> and <<B2>>"
+    result = dc_1.deref_text(text)
     assert result == "sum_val and 20"
 
     text = "{{A2}} and <<B2>> and <<a1>>"
-    result = dc.deref_text(text)
+    result = dc_1.deref_text(text)
     assert result == "{{A2}} and 20 and <<a1>>"
 
     text = "<<B2>>"
-    result = dc.deref_text(text, parser=IntParser())
+    result = dc_1.deref_text(text, parser=IntParser())
     assert result == 20
 
 
-def test_deref_failed():
-    sheet_name = "TestSheet"
-    workbook = openpyxl.load_workbook(join(dirname(__file__), '../../sample/test/deref/deref_template.xlsx'))
+def test_deref_failed(dc_1: DerefCell):
     text = "<<ZZZ000>>"
-
-    dc = DerefCell(sheet_name=sheet_name,
-                   workbook=workbook)
-
-    result = dc.deref_text(text)
+    result = dc_1.deref_text(text)
     assert result == text
 
 
