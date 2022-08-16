67d14fb9ef5c8e77f937881dadd7c880f54d0df5
Suchanuch Piriyasatit <p.suchanuch@thegang.tech>
2022-08-16 10:36:38 +0700

    add sheet name checker to handle different sheet name from template

--- src/exco/shortcut.py
+++ src/exco/shortcut.py
@@ -1,5 +1,9 @@
 from exco import ExcelProcessorFactory
+from exco.sheet_name_alias import SheetNameAliasCheckers
+from typing import Optional
 
 
-def from_excel(fname: str):
-    return ExcelProcessorFactory.default().create_from_template_excel(fname)
+def from_excel(fname: str, sheet_name_checkers: Optional[SheetNameAliasCheckers] = None):
+    return ExcelProcessorFactory.default().create_from_template_excel(fname=fname,
+                                                                      sheet_name_checkers=sheet_name_checkers
+                                                                      )
