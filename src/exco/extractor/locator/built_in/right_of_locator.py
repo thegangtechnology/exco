411f6e046a4723e76ade2e4ea95b30cb88bff1e2
lazythomas0147 <65774362+ThomasHarris0147@users.noreply.github.com>
2022-08-23 16:38:21 +0700

    simple simplification

--- src/exco/extractor/locator/built_in/right_of_locator.py
+++ src/exco/extractor/locator/built_in/right_of_locator.py
@@ -18,9 +18,7 @@ class RightOfLocator(Locator):  # TODO: Add search scope
         for row in sheet.iter_rows():
             for cell in row:
                 if cell.value == self.label:
-                    coord = cell.coordinate
-                    if util.is_merged_cell(sheet, cell.coordinate):
-                        coord = util.get_rightmost_coordinate(sheet=sheet, cell=cell)
+                    coord = util.get_rightmost_coordinate(sheet=sheet, cell=cell)
                     cell_loc = CellLocation(
                         sheet_name=anchor_cell_location.sheet_name,
                         coordinate=util.shift_coord(util.tuple_to_coordinate(coord[0], coord[1]),
