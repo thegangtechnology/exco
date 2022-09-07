b23799b9aa77ce7f71f81e52dbe4f059bde839a3
Rizkee Sakundee <s.rizkee@thegang.tech>
2022-09-07 16:43:39 +0700

    added below of regex

---
+++ src/exco/extractor/locator/built_in/below_of_regex_locator.py
@@ -0,0 +1,30 @@
+import re
+from dataclasses import dataclass
+
+from exco import CellLocation, util
+from exco.extractor.locator.locating_result import LocatingResult
+from exco.extractor.locator.locator import Locator
+from openpyxl import Workbook
+from openpyxl.worksheet.worksheet import Worksheet
+
+
+@dataclass(frozen=True)
+class BelowOfRegexLocator(Locator):
+    regex: str
+    n: int = 1
+
+    def locate(self, anchor_cell_location: CellLocation,
+               workbook: Workbook) -> LocatingResult:
+        sheet: Worksheet = workbook[anchor_cell_location.sheet_name]
+        compiled_regex = re.compile(self.regex)
+        for row in sheet.iter_rows():
+            for cell in row:
+                if compiled_regex.fullmatch(str(cell.value)) is not None:
+                    coord = util.get_rightmost_coordinate(sheet=sheet, cell=cell)
+                    cell_loc = CellLocation(
+                        sheet_name=anchor_cell_location.sheet_name,
+                        coordinate=util.shift_coord(coord.coordinate, (self.n, 0))
+                    )
+                    return LocatingResult.good(cell_loc)
+        return LocatingResult.bad(
+            msg=f"Unable to find cell below of {self.regex}")
