4e029fe429fd0b4449d13c37107390a3a0b85321
ThomasHarris0147 <thomasharris0147@gmail.com>
2023-04-25 14:42:38 +0700

    corrected code smells

--- tests/extractor/test_search_right_of_locator.py
+++ tests/extractor/test_search_right_of_locator.py
@@ -6,6 +6,8 @@ from exco.cell_full_path import CellFullPath
 from exco.extractor.locator.built_in.search_right_of_locator import SearchRightOfLocator
 from exco.extractor.locator.locating_result import LocatingResult
 
+A3_CELL_VALUE = 'the key 1'
+
 
 @pytest.fixture
 def wb() -> CellFullPath:
@@ -13,13 +15,13 @@ def wb() -> CellFullPath:
 
     ws = wb.active
     ws['D3'] = 40
-    ws['A3'] = 'the key 1'
+    ws['A3'] = A3_CELL_VALUE
 
     return wb
 
 
 def test_search_right_of_locator(wb: Workbook):
-    rol = SearchRightOfLocator(label='the key 1', max_empty_col_search=5)
+    rol = SearchRightOfLocator(label=A3_CELL_VALUE, max_empty_col_search=5)
     result = rol.locate(anchor_cell_location=CellLocation(
         sheet_name="Sheet",
         coordinate="A3"
@@ -32,7 +34,7 @@ def test_search_right_of_locator(wb: Workbook):
 
 
 def test_search_right_of_locator_max(wb: Workbook):
-    rol = SearchRightOfLocator(label='the key 1', max_empty_col_search=2)
+    rol = SearchRightOfLocator(label=A3_CELL_VALUE, max_empty_col_search=2)
     result = rol.locate(anchor_cell_location=CellLocation(
         sheet_name="Sheet",
         coordinate="A3"
